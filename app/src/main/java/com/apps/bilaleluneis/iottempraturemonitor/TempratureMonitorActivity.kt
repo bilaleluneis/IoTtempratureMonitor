f4fea85d716d4176b7d61263cefd27692b75b9fa
bilaleluneis <bilaleluneis@gmail.com>
2018-02-12 20:25:06 -0500

    update to use property observer to clear display!

--- app/src/main/java/com/apps/bilaleluneis/iottempraturemonitor/TempratureMonitorActivity.kt
+++ app/src/main/java/com/apps/bilaleluneis/iottempraturemonitor/TempratureMonitorActivity.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import com.google.android.things.contrib.driver.bmx280.Bmx280
 import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay
 import com.google.android.things.contrib.driver.rainbowhat.RainbowHat
+import kotlin.properties.Delegates
 
 /**
  * @author Bilal El Uneis
@@ -19,14 +20,25 @@ import com.google.android.things.contrib.driver.rainbowhat.RainbowHat
 
 class TempratureMonitorActivity : Activity() {
 
-    /* wanted to use TempratureMonitorActivity::javaClass.toString()
-       but Kotlin reflection is not available I guess when doing embedded
-       programming */
     private val logTag = "TempratureMonitorActivity"
-
     private val tempratureSensor by lazy { initTempratureSensor() }
     private val display by lazy { initDisplay() }
     private val looper = Handler(Looper.getMainLooper()) //TODO: read about this, and find what is better
+    /**
+     This is cool! an observable property , similar to Swift willSet and didSet
+     the property initial value is 0, I used _ for first two arguments in Lambda
+     as i don't care for property and oldValue parameters and only need the newValue
+     when I call this property with ++ and it reaches value of 2 I set it back to 0
+     and clear the display.
+     check out the counterToClearDisplay++ in [backgroundLooper]
+     */
+    private var counterToClearDisplay : Int by Delegates.observable(0){
+        _, _, newValue ->
+        if(newValue == 2) {
+            counterToClearDisplay = 0
+            display.clear()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -41,14 +53,14 @@ class TempratureMonitorActivity : Activity() {
      * need to look into activity LC to understand what else i might need to
      * override for clean up and shutdown
      */
-    //TODO: is the sequence correct? i destroy this props then super?
-    //TODO: how do I turn off the display when i kill process in IDE?!!
     override fun onDestroy() {
 
+        super.onDestroy()
         Log.d(logTag, "Inside OnDestroy!")
+        display.clear()
+        display.setEnabled(false)
         display.close()
         tempratureSensor.close()
-        super.onDestroy()
 
     }
 
@@ -90,10 +102,14 @@ class TempratureMonitorActivity : Activity() {
         Log.d(logTag, "executing backgroundLooper!")
         val temprature = toFahrenheit(tempratureSensor.readTemperature())
         display.display(temprature.toString())
-        looper.postDelayed(this::backgroundLooper, 500)
+        counterToClearDisplay ++
+        //repeat every 10 seconds!
+        looper.postDelayed(this::backgroundLooper, 10000)
 
     }
 
+    //TODO: need to debug this.. it is returning higher value than what should be correct!
+    //or is it the hardware Sensor or driver that is reading higher number than it is?!!
     private fun toFahrenheit(celsius: Float) : Float = ((celsius * 1.8) + 32).toFloat()
 
 }
